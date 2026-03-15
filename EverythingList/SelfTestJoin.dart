fd4753840d6e92b35ab1cc6e266df9cedadc77f6
mdrabbi <rabbihmgc2023@gmail.com>
2026-03-15 23:30:01 +0600

    Merge remote-tracking branch 'origin/main'

--- EverythingList/SelfTestJoin.dart
+++ EverythingList/SelfTestJoin.dart
@@ -1,6 +1,6 @@
 void main(){
   List<String> mylist = ["My Name", "is","Rabbi"];
-
+  String mylist1 = mylist.join();
   print(mylist1);
   String mysecondlist = mylist.join(" ");
   print(mysecondlist);
