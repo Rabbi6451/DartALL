4ca6adde350ad679a0fb4827df87701c367ab216
mdrabbi <rabbihmgc2023@gmail.com>
2026-03-18 23:41:06 +0600

    Merge remote-tracking branch 'origin/main'
    # Conflicts:
    #	EverythingList/Practice.dart

---
+++ EverythingSet/second.dart
@@ -0,0 +1,10 @@
+void main()
+{
+  var gfg = ['Geeks','For','Geeks'];
+
+  print("Output of the list is: $gfg");
+
+  var gfg1 = <String>{'Geeks','For','Geeks'};
+
+  print("Output of the set is: $gfg1");
+}
