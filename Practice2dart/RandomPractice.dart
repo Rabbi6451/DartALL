c53fd5429c3ea8ec6beca15397372972d3f262a4
mdrabbi <rabbihmgc2023@gmail.com>
2026-03-23 23:31:57 +0600

    Merge remote-tracking branch 'origin/main'
    # Conflicts:
    #	EverythingList/Practice.dart

--- Practice2dart/RandomPractice.dart
+++ Practice2dart/RandomPractice.dart
@@ -2,12 +2,10 @@ class Student {
   String? name;
   int? age;
   int? rollNumber;
-  // Constructor
   Student(this.name, this.age, this.rollNumber);
 }
 
 void main(){
-  // Here student is object of class Student.
   Student student = Student("John", 20, 1);
   print("Name: ${student.name}");
   print("Age: ${student.age}");
