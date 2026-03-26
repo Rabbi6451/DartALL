80a8be4a4d6a1e5e72b322b5d894d5aa5cadbee2
mdrabbi <rabbihmgc2023@gmail.com>
2026-03-26 23:17:24 +0600

    Merge remote-tracking branch 'origin/main'
    # Conflicts:
    #	EverythingList/Practice.dart

---
+++ EveryThingNull/TypePromotion2.dart
@@ -0,0 +1,10 @@
+void main(){
+  String result;
+  if(DateTime.now().hour < 12) {
+    result = "Good Morning";
+  } else {
+    result = "Good Afternoon";
+  }
+  print("Result is $result");
+  print("Length of result is ${result.length}");
+}
