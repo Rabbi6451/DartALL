2bb9033715385f01f88b7f5337398d5214524f96
mdrabbi <rabbihmgc2023@gmail.com>
2026-04-04 23:22:47 +0600

    Merge remote-tracking branch 'origin/main'
    # Conflicts:
    #	EverythingList/Practice.dart

---
+++ FunctionInDart/PracticeSecondFunction.dart
@@ -0,0 +1,11 @@
+void second1(double first, double second, double third){
+  double average = first + second + third;
+  print("The average is $average");
+}
+void main(){
+  double first = 100;
+  double second = 200;
+  double third = 300;
+
+  second1(first,second,third);
+}
