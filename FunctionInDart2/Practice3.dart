29ff2f938ec8e6a8ef7522ac5c229eec074d6891
mdrabbi <rabbihmgc2023@gmail.com>
2026-04-06 23:32:37 +0600

    Merge remote-tracking branch 'origin/main'
    # Conflicts:
    #	EverythingList/Practice.dart

---
+++ FunctionInDart2/Practice3.dart
@@ -0,0 +1,28 @@
+
+import 'dart:io';
+
+int sum(int first, int second){
+  return first+second;
+
+}
+double div(int first, int second){
+  return first/second;
+
+}
+int mul(int first, int second){
+  return first*second;
+
+}
+void main(){
+  print("Enter your first number : ");
+  double first = double.parse(stdin.readLineSync()!);
+  print("Enter you second number : ");
+  double second = double.parse(stdin.readLineSync()!);
+  print("Enter do you want ? ");
+  String? chose = stdin.readLineSync();
+  if(chose == '+'){
+    print(sum(first.toInt(),second.toInt()));
+  }else{
+    print("Invalid ");
+  }
+}
