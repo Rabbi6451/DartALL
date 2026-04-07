8bc63939b4799bf0d304f9c3104f71f3a811bfa9
mdrabbi <rabbihmgc2023@gmail.com>
2026-04-07 19:16:40 +0600

    Merge remote-tracking branch 'origin/main'
    # Conflicts:
    #	EverythingList/Practice.dart

--- CollectionOfDart(SetListMap)Practice/Practice1(Set).dart
+++ CollectionOfDart(SetListMap)Practice/Practice1(Set).dart
@@ -1,7 +1,17 @@
-
 void main(){
   Set<int> input ={4,5,6};
   for(int input1 in input){
     print(input1);
   }
+  print("------------------");
+  List<int> input2 = [4,5,6,7];
+  for(int input3 in input2){
+    print(input3);
+  }
+  Map<String , String> input3 = {
+    'first' : 'Dhaka',
+    'second' : 'Chandpur',
+    'third' : 'hajigonj'
+  };
+  print(input3);
 }
