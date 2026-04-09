d965f2b158a5cad5e805b55c9d77f40d4120cc40
mdrabbi <rabbihmgc2023@gmail.com>
2026-04-09 12:47:12 +0600

    Merge remote-tracking branch 'origin/main'
    # Conflicts:
    #	EverythingList/Practice.dart

---
+++ MathIndart/FirstMath.dart
@@ -0,0 +1,17 @@
+import 'dart:math';
+
+void main(){
+  int first = 50;
+  int second = 50;
+
+  num value1 = pow(first,second);
+  num value2 = min(first,second);
+  num value3 = max(first,second);
+  num value4 = sqrt(first);
+
+  print("Power of two number is $value1");
+  print("Minimum number between two number is $value2");
+  print("Maximum number between two number is $value3");
+  print("Squire of the number is $value4");
+
+}
