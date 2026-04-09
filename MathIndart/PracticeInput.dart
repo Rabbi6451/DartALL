0e61a2c6552cf1e90a571b073abdfda4284629f9
mdrabbi <rabbihmgc2023@gmail.com>
2026-04-09 23:55:42 +0600

    Merge remote-tracking branch 'origin/main'
    # Conflicts:
    #	EverythingList/Practice.dart

---
+++ MathIndart/PracticeInput.dart
@@ -0,0 +1,13 @@
+import 'dart:io';
+
+void main(){
+  print("Enter your number : ");
+  String? input = stdin.readLineSync();
+  if(input == 10 ){
+    for(int i=0;i<5;i++){
+      print("Hello");
+    }
+  }else{
+    print("Your input is not correct to print");
+  }
+}
